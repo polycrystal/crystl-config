3b22db95f601cb89748a97effb86191d790c79d1
devagustinprado <86616429+devagustinprado@users.noreply.github.com>
2022-01-26 23:42:12 -0300

    added cronos crona crystl-cro vault

--- vaults/cronos.ts
+++ vaults/cronos.ts
@@ -1,1 +1,23 @@
-export const cronos = [];
+export const cronos = [
+  {
+    id: "crona-wcro-crystl",
+    pid: 44,
+    lpSymbol: "CRYSTL-CRO LP",
+    lpProvider: "Crona",
+    wantAddress: "0xdEb28305D5c8d5Ce3B3bc5398Ba81012580a5A11",
+    depositFee: "0%",
+    strategyAddress: "0xCb62Ca7E0BccDc875D7aE68cd5Ca63d010534b52",
+    pricePerFullShare: 1,
+    tvl: 0,
+    oracle: "lps",
+    oracleId: "crona-wcro-crystl",
+    oraclePrice: 0,
+    paused: false,
+    platform: "Crona",
+    farmSite: "https://app.cronaswap.org/",
+    projectSite: "https://www.crystl.finance/",
+    assets: ["CRYSTL", "CRO"],
+    addLiquidityUrl:
+      "https://app.cronaswap.org/add/0x5C7F8A570d578ED84E63fdFA7b1eE72dEae1AE23/0xCbDE0E17d14F49e10a10302a32d17AE88a7Ecb8B",
+  },
+];
